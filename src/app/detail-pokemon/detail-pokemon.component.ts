f2f6593eb6ddecabbbfee40c691e668b15bfba84
eliewebavenue <elie@webavenue.agency>
2024-04-17 12:33:38 +0200

    Maj

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Pokemon } from '../pokemon.models';
+import { ActivatedRoute, Router } from '@angular/router';
+import { POKEMONS } from '../api-pokemons';
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -7,5 +10,21 @@ import { Component } from '@angular/core';
   ]
 })
 export class DetailPokemonComponent {
+  pokemonList: Pokemon[] = POKEMONS
+  pokemon: Pokemon|undefined;
+
+  constructor(private route:ActivatedRoute, private router: Router) {}
+
+  ngOnInit(){
+    //ActivatedRoute est une classe de Angular qui permet de recuperer toutes les informations a propos de la route actif. Ici, la route est : pokemon/:id, donc avec paramMap.get('id') je recupere l'id passé sur l'url
+    const pokemonId: string | null = this.route.snapshot.paramMap.get('id');
+    if(pokemonId){
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    }
+  }
+
+  goBack(): void {
+    this.router.navigate(['pokemons'])
+  }
 
 }
