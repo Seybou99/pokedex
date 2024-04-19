70edfc5875776b0c89c1d26b5831489b5df841c5
eliewebavenue <elie@webavenue.agency>
2024-04-19 10:30:51 +0200

    auth service

---
+++ src/app/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable, delay, of, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  isLoggedIn: boolean = false;
+
+  constructor() { }
+
+  login(name:string, password:string): Observable<boolean> {
+
+    /**
+     * Normalement ici on a une requete POST vers le serveur en envoyant le name et le mdp 
+     * Ensuite, l'API va checker si le name et le mdp existe et si c'est le bon, 
+     * et en fonction de cela l'API retournera true ou false
+     */
+
+    let isLoggedIn = false; 
+    if(name == 'pikachu' && password == 'carapuce'){
+      isLoggedIn = true;
+    }
+
+    return of(isLoggedIn).pipe(
+      delay(1000), 
+      tap(isLoggedIn => this.isLoggedIn = isLoggedIn)
+    )
+  }
+}
